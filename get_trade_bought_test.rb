871821d7b571a15c57470b68ce03e2810a7ffcde
shikaifeng <kaifeng15@163.com>
2014-05-22 15:45:32 +0800

    get_trade_fullinfo_test

--- get_trade_bought_test.rb
+++ get_trade_bought_test.rb
@@ -15,7 +15,7 @@ RSpec.describe "Thrift::Api::V1::Test" do
           describe "normal Test" do
             before :all do
                 @t=Thrift::Api::V1::Test.new
-                session = "6100506425279d184141918e66dd4e6368eca7e24a1e92489310901"
+                session = "6102009ea3a81cc241340733a25272b0710e381187960222061700319"
                 opts={"fields" => "seller_nick, buyer_nick, title, type, created, sid, tid, seller_rate, buyer_rate, can_rate, status, payment, discount_fee"}
                 @data = @t.taobao_top.get_trade_bought(session, opts)
                 @obj= JSON.parse(@data)
--- get_trade_fullinfo_test.rb
+++ get_trade_fullinfo_test.rb
@@ -19,12 +19,57 @@ RSpec.describe "Thrift::Api::V1::Test" do
                 opts={"fields" => "seller_nick, buyer_nick, title, type, created", "tid" => "653362338620109"}
                 @data = @t.taobao_top.get_trade_fullinfo(session, opts)
                 @obj= JSON.parse(@data)
-                puts @obj
             end 
-            #1.valid itemcats values
-            it "valid the values of the itemcats returned right" do
-                puts @obj
-
+            #1.valid values
+            it "valid the values of the trade fullinfo returned right" do
+                @obj["trade_fullinfo_get_response"].count.should eq 1
+                @obj["trade_fullinfo_get_response"]["trade"].count.should eq 5 
+                @obj["trade_fullinfo_get_response"]["trade"]["buyer_nick"].should eq "kaifeng15"
+                @obj["trade_fullinfo_get_response"]["trade"]["seller_nick"].should eq "周生生官方旗舰店"
+                @obj["trade_fullinfo_get_response"]["trade"]["title"].should eq "周生生官方旗舰店"
+                @obj["trade_fullinfo_get_response"]["trade"]["type"].should eq "fixed"
+            end
+        end
+        # unexpection test   
+        describe "unexpection test" do
+            before :all do
+                @t=Thrift::Api::V1::Test.new
+            end
+            #2.session is blank,return error
+            it "when session is blank,return error" do
+                session = ""
+                opts={"fields" => "seller_nick, buyer_nick, title, type, created", "tid" => "653362338620109"}
+                @data = @t.taobao_top.get_trade_fullinfo(session,opts)
+                @obj= JSON.parse(@data)
+                @obj["error_response"]["code"].should eq 27
+                @obj["error_response"]["msg"].should eq "Invalid session:session-is-blank"
+            end
+            #3.session is error,return error
+            it "when session is blank,return error" do
+                session = "123123123123"
+                opts={"fields" => "seller_nick, buyer_nick, title, type, created", "tid" => "653362338620109"}
+                @data = @t.taobao_top.get_trade_fullinfo(session,opts)
+                @obj= JSON.parse(@data)
+                @obj["error_response"]["code"].should eq 27
+                @obj["error_response"]["msg"].should eq "Invalid session"
+            end
+            #4.tid not excised,return none
+            it "when tid not excised,return none" do
+                session = "6100506425279d184141918e66dd4e6368eca7e24a1e92489310901"
+                opts={"fields" => "seller_nick, buyer_nick, title, type, created", "tid" => ""}
+                @data = @t.taobao_top.get_trade_fullinfo(session,opts)
+                @obj= JSON.parse(@data)
+                @obj["error_response"]["code"].should eq 40
+                @obj["error_response"]["msg"].should eq "Missing required arguments:tid"
+            end
+            #5.tid not excised,return none
+            it "when tid not excised,return none" do
+                session = "6100506425279d184141918e66dd4e6368eca7e24a1e92489310901"
+                opts={"fields" => "seller_nick, buyer_nick, title, type, created", "tid" => "65336"}
+                @data = @t.taobao_top.get_trade_fullinfo(session,opts)
+                @obj= JSON.parse(@data)
+                @obj["error_response"]["code"].should eq 15
+                @obj["error_response"]["sub_msg"].should eq "订单号不存在"
             end
         end
     end
